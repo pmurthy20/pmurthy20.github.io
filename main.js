0f882686015e7bada3f3e324123261731501e19b
Prathik M <murthy.prathik@gmail.com>
2021-03-27 15:11:25 -0400

    new resume

--- main.js
+++ main.js
@@ -1,103 +1,168 @@
 
 
-var c = document.getElementById("canvas");
-ctx = c.getContext("2d");
-var move = 5;
-var x = 400;
-var y = 400;
-var timer = 0;
-framerate = 5;
-enabled = true;
-grow = false;
-var segments = [];
-var xlist = [];
-var ylist = [];
-var length = 1;
-xlist[0] = 400;
-ylist[0] = 400;
-ctx.fillRect(xlist[0], ylist[0], 20, 20);
+API_KEY = "34fc7946";
+NAME = "Death Note";
+url = 'http://www.omdbapi.com/?t='+NAME+'&apikey='+API_KEY;
+shows = [];
+episodes = [];
+x = 1;
+SEASON = 1;
+SEASON_MAX = 0;
+EPISODE_BIG = [];
+EPISODE_SMALL = [];
+RATINGS = [];
+EPISODE_COUNT = 1;
+EPISODE_INDEX= [];
 
-window.requestAnimationFrame(gameLoop);
-function gameLoop() {
-    if (timer <= framerate) {
-        timer++;
-    } else {
-        moveHandler();
-        timer = 0;
-    }
-    window.requestAnimationFrame(gameLoop);
+
+
+function apiCall() {
+    // $.getJSON('http://www.omdbapi.com/?s='+NAME+'&apikey='+API_KEY).then((response) => {
+    //    console.log(response) 
+    //    RETURN = response;
+    // });
+    
+    $.getJSON(url, function(result) {
+        $.each(result, function(i, field){
+            if (typeof field == "object") {
+            $.each(field, function(content) {
+                // console.log(field[content]);
+                shows[shows.length] = field[content]["imdbID"];
+                if (x < field.length) {
+                    x++;
+                } else {
+                    display();
+                }
+            })
+            }
+        })
+    })
+
+
 }
 
+function display() {
+    for (i = 0; i < shows.length; i++) {
+        // console.log(shows[i]);
+        $.getJSON('http://www.omdbapi.com/?i='+shows[i]+'&apikey='+API_KEY, function(result) {
+            console.log(result);
+        })
+    }
+}
 
-function updateDir() {
-    if (length > 1) {
-        for (let z = 0; z < length; z++) {
-            segments[z+1] = segments[z];
+function getTotalSeasons() {
+    $.getJSON(url, function(result) {
+            console.log(result);
+            SEASON_MAX = result["totalSeasons"]
+            getEpisodes();
+        })
+}
+
+function getEpisodes() {
+    SEASON_COUNT = 1;
+    for (SEASON = 1; SEASON <= SEASON_MAX; SEASON++) {
+        $.getJSON('http://www.omdbapi.com/?t='+NAME+'&Season='+SEASON+'&apikey='+API_KEY, function(result) {
+            console.log(result);
+            // console.log(result["Response"]);
+            
+            EPISODE_BIG[result["Season"]-1] = result["Episodes"];
+            SEASON_COUNT++;
+
+            if (SEASON_COUNT > SEASON_MAX) {
+                // console.log(EPISODE_BIG);
+                displayEpisodes();
+            }   
+    });
+    }
+}
+
+function displayEpisodes() {
+    for (y = 0; y < EPISODE_BIG.length; y++) {
+        // console.log(EPISODE_BIG[y]);
+        for (z = 0; z < EPISODE_BIG[y].length; z++) {
+            // console.log(EPISODE_BIG[y][z]["imdbRating"]);
+            RATINGS.push(EPISODE_BIG[y][z]["imdbRating"]);
+            EPISODE_INDEX.push(EPISODE_COUNT++)
+            if (z == EPISODE_BIG[y].length-1) {
+                // console.log(RATINGS);
+                for (x = 0; x < RATINGS.length; x++) {
+                    console.log(RATINGS[x]);
+                }
+                // var chart = new Chart(ctx, {
+                //     type: 'line',
+                //     data: RATINGS,
+                //     options
+                // })
+                chart();
+            }
+
         }
-        segments[0] = move;
-    } 
-    segments[0] = move;
+    }
 }
 
-function draw() {
-    for (let z = 0; z < length; z++) {
-        var segmove = segments[z];
-        switch (segmove) {
-            case 0:
-                // console.log(segments);
-                ctx.clearRect(xlist[z], ylist[z], 20, 20);
-                xlist[z] -= 20;
-                ctx.fillRect(xlist[z], ylist[z], 20, 20);
-                break;
-            case 1:
-                ctx.clearRect(xlist[z], ylist[z], 20, 20);
-                ylist[z] -= 20;
-                ctx.fillRect(xlist[z], ylist[z], 20, 20);
-                break;
-            case 2:
-                ctx.clearRect(xlist[z], ylist[z], 20, 20);
-                xlist[z] += 20;
-                ctx.fillRect(xlist[z], ylist[z], 20, 20);
-                break;
-            case 3:
-                ctx.clearRect(xlist[z], ylist[z], 20, 20);
-                ylist[z] += 20;
-                ctx.fillRect(xlist[z], ylist[z], 20, 20);
-                break;
+function chart() {
+    // var ctx = document.getElementById("chart-js");
+    // new chart(ctx , {
+    //     type: 'line',
+    //     data: RATINGS,
+    //     options: {}
+    // });
+    // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,24, 25, 26, 27, 28, 29, 30, 31, 32, 33
+    title = document.getElementById('title');
+    title.innerHTML = NAME;
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: EPISODE_INDEX,
+            datasets: [{
+                label: 'Rating out of 10',
+                data: RATINGS,
+                // backgroundColor: [
+                //     'rgba(255, 99, 132, 0.2)',
+                //     'rgba(54, 162, 235, 0.2)',
+                //     'rgba(255, 206, 86, 0.2)',
+                //     'rgba(75, 192, 192, 0.2)',
+                //     'rgba(153, 102, 255, 0.2)',
+                //     'rgba(255, 159, 64, 0.2)'
+                // ],
+
+                // ],
+                borderWidth: 4,
+                borderColor: "rgba(0, 230, 142, 0.5)",
+                // backgroundColor:  "rgba(255, 255, 255, 0)",
+                // pointHoverBackgroundColor: ['rgba(75, 192, 192, 0.2)'],
+                // pointHoverRadius: 20,
+
+            }],
+            
+            
+        },
+        options: {
+            layout: {
+                // padding: {
+                //     left: 50,
+                //     right: 50,
+                //     top: 5000,
+                //     bottom: 50
+                // }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        max: 10
+                    }
+                }]
+            }
         }
-    }
+        // options: {}
+    });
+
+    
+
 }
 
-function updatePos() {
-
-}
-
-function moveHandler() {
-   updateDir();
-   console.log(segments);
-   updatePos();
-   draw();
-}
-
-document.addEventListener('keydown', function(event) {
-    if(event.keyCode == 39) {
-        move = 2;
-        // length += 1;
-        // console.log(length);
-    }
-    else if(event.keyCode == 37) {
-        move = 0;
-    }
-    else if(event.key == "ArrowUp") {
-        move = 1;
-    }
-    else if(event.key == "ArrowDown") {
-        move = 3;
-    }
-    else if (event.keyCode == 32) {
-        length += 1;
-        console.log(length);
-    }
-});
 
 
+getTotalSeasons();
